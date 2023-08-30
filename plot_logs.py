c851a5ee4ae75b4e5484b85eb8c93bd5cea9d78a
filippo <filibiondi2000@gmail.com>
2023-08-30 16:45:10 +0200

    added new test for queue

--- plot_logs.py
+++ plot_logs.py
@@ -3,31 +3,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def main(file_name):
-    table = pd.read_csv(file_name, sep='\t')
-    # time = table['Times']
-    # throughput = table[:, 3]
-    # arrival_freq = table[:, 4]
-    # queue_size = table[:, 5]
-    # n_workers = table[:, 6]
+def main(file_names):
+    for file_name in file_names:
+        table = pd.read_csv(file_name, sep='\t')
 
-    fig, axs = plt.subplots(4)
+        fig, axs = plt.subplots(4)
 
-    axs[0].set_title('Throughput')
-    axs[0].plot(table["Timestamp"], table["Average_throughput"])
-    axs[0].plot(table["Timestamp"], table["Average_arrival_frequency"])
+        axs[0].set_title('Throughput')
+        axs[0].plot(table["Timestamp"], table["Average_throughput"])
+        axs[0].plot(table["Timestamp"], table["Average_arrival_frequency"])
 
-    axs[1].set_title('Queue Size')
-    axs[1].plot(table["Timestamp"], table["Queue_size"])
+        axs[1].set_title('Queue Size')
+        axs[1].plot(table["Timestamp"], table["Queue_size"])
 
-    axs[2].set_title('Latency')
-    axs[2].plot(table["Timestamp"], table["Average_worker_latency"])
+        axs[2].set_title('Latency')
+        axs[2].plot(table["Timestamp"], table["Average_worker_latency"])
 
-    axs[3].set_title('N. Workers')
-    axs[3].plot(table["Timestamp"], table["n_workers"])
+        axs[3].set_title('N. Workers')
+        axs[3].plot(table["Timestamp"], table["n_workers"])
 
-    plt.show()
+        plt.show()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1:])
---
+++ test/native/test_queue_recovery.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+
+#include "AutonomicFarm.hpp"
+
+
+int test_func(int x)
+{
+	auto start_time = high_resolution_clock::now();
+	while(duration<double>(high_resolution_clock::now() - start_time).count() < 0.5)
+	{
+		x++;
+	}
+	return x;
+}
+
+int main()
+{
+	std::ofstream log_file;
+	log_file.open("../test_queue.txt");
+
+	AutonomicFarm farm{false,
+	                   5,
+	                   2,
+	                   10,
+	                   1000,
+	                   2.0,
+	                   0.5,
+	                   100,
+	                   10,
+	                   10.0,
+	                   5.0,
+	                   2.0,
+	                   MonitorLogger{{&log_file}, true}};
+
+	farm.start();
+
+	for (int i = 0; i < 100; i++)
+	{
+		int input = 0;
+		farm.add_task<int, int>(test_func, input);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+
+	for (int i = 0; i < 1000; i++)
+	{
+		int input = 0;
+		farm.add_task<int, int>(test_func, input);
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+
+	for (int i = 0; i < 100; i++)
+	{
+		int input = 0;
+		farm.add_task<int, int>(test_func, input);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+
+	farm.stop();
+
+	for(int i = 0; i < 10; i++)
+	{
+		int result = farm.get_result<int>();
+		std::cout << result << std::endl;
+	}
+
+	log_file.close();
+	return 0;
+}
